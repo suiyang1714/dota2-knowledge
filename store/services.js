c951eb2465e3687b0dad8aa164444caff1aab19a
suiyang <171426589@qq.com>
2018-08-02 17:08:34 +0800

    请求ip更改

--- store/services.js
+++ store/services.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://127.0.0.1:7278/'
+// const baseUrl = 'http://127.0.0.1:7278/'
+const baseUrl = 'https://dota2.adityasui.com'
 class Services {
   newQuestion(issue) {
     return axios.post(`${baseUrl}api/question/new`, issue)
